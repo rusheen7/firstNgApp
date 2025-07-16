9d17cf136950c430a935cae1bf689069bf4b8ac6
Rusheen Patel <rusheen7@users.noreply.github.com>
2025-07-16 19:59:12 -0400

    Data binding, passing data from parent to child, event listener, counting component, and creating routes

---
+++ src/app/home/home.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
+import { Counter } from '../components/counter/counter';
+
+@Component({
+  selector: 'app-home',
+  imports: [Greeting, Counter],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+  homeMessage = signal('Hello, world!');
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
+}
