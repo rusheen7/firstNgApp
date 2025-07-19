6c08d9d7a2ab27c22b33904e3be896d92dbacaaf
Rusheen Patel <rusheen7@users.noreply.github.com>
2025-07-18 22:27:47 -0400

    Angular directives

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Todo } from '../../model/todo.type';
+import { HighlightCompletedTodo } from '../../directives/highlight-completed-todo';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodo],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss'
 })
 export class TodoItem {
+  todo = input.required<Todo>();
+  todoToggled = output<Todo>();
 
+  todoClicked() {
+    this.todoToggled.emit(this.todo());
+  }
 }
--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItem } from '../components/todo-item/todo-item';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [TodoItem],
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
@@ -24,6 +25,21 @@ export class Todos implements OnInit {
     )
     .subscribe((todos) => {
       this.todoItems.set(todos);
+    });
+  }
+
+
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map(todo => {
+        if (todo.id == todoItem.id) {
+          return {
+            ... todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
     });
   }
 }
